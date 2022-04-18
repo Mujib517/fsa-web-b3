d1f432e9cda6c121631fc322fe78bb69edc03b45
Mujib <mujib517@gmail.com>
2022-04-18 09:42:48 +0530

    js day1

---
+++ index.js
@@ -0,0 +1,54 @@
+// Programming language
+// loosely typed lang
+// run on browser
+// mobile, desktop, IOT, browsers, AI&ML
+// Javascript, Python
+// install
+// long term support 3 years
+// bugs, security issues
+// byte code
+// Concepts
+// PPL
+// variables: memory location, temporary
+// RAM: random access memory
+// 64k, processor
+// internals
+// assembly code
+// high level
+// 2.4*4 Cores Ghz frequency, Cycles per second 
+// 10^9 instructions per second
+// DAA Notations Big Oh O(N)
+// O(LogN)
+// OS
+// overclock
+// 1Ghz = 1.5Ghz
+// heat
+// 2 bit os
+// 3 bit os: 2^3-1
+// 32 bit: 2^32 : 4GB
+// 64 bit : 2^64 
+// 128 bit
+/*
+ 000
+ 001
+ 010
+ 011
+*/
+/*
+    0 0
+    0 1
+    1 0
+    1 1
+*/
+var a = 10; // 64bits
+int a = 10; // 32bit: 4 bytes
+int a = 10; // c: 16bits
+boolean flag = true; // 8bits, 0000 0000 or 0000 0001
+var b = 20;
+var c = a + b; 
+// Garbage collection
+// x86 architecture CISC
+// ARM RISC mobile less power, less powerful
+// console.log(c); // interpolation
+// console.log("hello world");
+// console.log('hello world');
