1454b85227b2f466370d4b35dfe8916c656c3948
Mujib <mujib517@gmail.com>
2022-04-20 08:17:53 +0530

    Forms

--- index3.js
+++ index3.js
@@ -2,14 +2,15 @@
 // artithmatic operators: +,-,*,/,%
 // conditional operators: <,>,<=,>=, ==, === (strict euqlity), !=, !==
 // Logical operators: &&, ||, !
+// bitwise operator
 // unary operators: ++, --, pre increment (increment first, assignemnt next), 
 // post increment (assignment first, increment next)
 
-var a = 10;
-var b = 21;
+// operands
+// var a = 10;
+// var b = 21;
 
-console.log(a > 10);
-
+// console.log(a > 10);
 // console.log(++a); //11
 // console.log(--b); //20
 // var x = a++; //10
@@ -22,3 +23,36 @@ console.log(a > 10);
 // console.log(a * b);
 // console.log(a / b);
 // console.log(b % a);
+
+
+var age = 30;
+
+// shorthand evaluation
+var isYoung = (age >= 20) || (age <= 40);
+
+console.log(isYoung);
+/*
+A   B   A&B     A|B
+F   F   F       F
+T   F   F       T
+F   T   F       T
+T   T   T       T
+*/
+
+/*
+A   !A  
+0   1
+1   0
+*/
+
+// var x = true;
+// console.log(!!x);
+
+// 0000 0001
+// 0000 0000
+// 0000 0000 0000
+var y = 1 << 5; // 2^x
+
+var z = 100000 >> 4;
+
+console.log(z);
