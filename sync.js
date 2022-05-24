c253b2e0f76c880d1ac2e0ccd034f7d2a2a05b0c
Mujib <mujib517@gmail.com>
2022-05-24 09:04:03 +0530

    timer funtions

---
+++ sync.js
@@ -0,0 +1,33 @@
+// synchronous javascript
+// async
+// 80% nodejs
+// list of items: grocery store
+// async: delay 
+// function add(a, b) {
+//     a++;
+//     b++;
+//     var c = a + b;
+//     return c;
+// }
+
+// var c = add(10, 20);
+// console.log(c);
+
+function fn() {
+    console.log("Fn()");
+}
+// setTimeout(fn, 1000);
+// setInterval(fn, 1000);
+// locale time
+// culture
+// 24-05-2022
+// 05-24-2022
+// Dollars
+// 25C
+// 50F
+// 20km
+// 15Miles
+// globalization 
+setInterval(function () {
+    console.log(new Date());
+}, 1000);
